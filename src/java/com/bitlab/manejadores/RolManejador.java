50ff7f8bbddc706a41f26272864c972bc9fd09dd
Cristian Aguilar <cristian.aguilar02@gmail.com>
2020-08-28 18:50:58 -0600

    crud rol

---
+++ src/java/com/bitlab/manejadores/RolManejador.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bitlab.manejadores;
+
+import com.bitlab.controladores.RolControlador;
+import com.bitlab.entidades.Rol;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Aguilar
+ */
+
+@ManagedBean
+@ViewScoped
+public class RolManejador extends ManejadorAbstracto<Rol>{
+
+    //controlador para la persistencia
+    private RolControlador rolControlador;
+    
+    public RolManejador() {
+        super(Rol.class);
+        rolControlador = new RolControlador();
+    }
+
+    @Override
+    public RolControlador obtenerControlador() {
+        return rolControlador;
+    }
+    
+}
